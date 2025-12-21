b8b0cdb949a16020a2a280ee7affd59e340bf4fd
Nicolas Ferré <nicolas.ferre.public@gmail.com>
2025-12-21 20:27:38 +0100

    Fix CI

--- .github/scripts/check_coverage.sh
+++ .github/scripts/check_coverage.sh
@@ -2,9 +2,9 @@
 set -xeu
 
 coverage=$(grep "<coverage" coverage.xml | grep -oP 'line-rate="\K[0-9.]+')
-failure=$(awk 'BEGIN{ print '"$coverage * 100"'<'"$COV_THRESHOLD"' }')
+failure=$(awk 'BEGIN{ print '"$coverage"'<'"1.0"' }')
 if [ "$failure" -eq "1" ]; then
-    echo "Coverage has failed with $coverage% instead of at least $COV_THRESHOLD%."
+    echo "Coverage rate has failed with $coverage (expected: 1.0)."
     exit 1
 else
     echo "Coverage has successfully passed with $coverage%."
