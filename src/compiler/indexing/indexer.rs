40064d20eaee6ef95b42ef1e4b8fee13f468ce7b
Nicolas Ferré <nicolas.ferre.public@gmail.com>
2026-05-02 11:00:32 +0200

    Fix function call resolution in return types

--- src/compiler/indexing/indexer.rs
+++ src/compiler/indexing/indexer.rs
@@ -4,6 +4,7 @@ use crate::compiler::parsing::exprs::Expr;
 use crate::compiler::parsing::exprs::calls::Call;
 use crate::compiler::parsing::exprs::idents::Ident;
 use crate::compiler::parsing::items::Item;
+use crate::compiler::parsing::items::fns::FnDefinition;
 use crate::compiler::parsing::modules::Module;
 use crate::compiler::parsing::statements::Statement;
 use crate::compiler::prelude::PRELUDE_FILE_INDEX;
@@ -13,12 +14,14 @@ use crate::utils::parsing::span::Span;
 #[derive(Debug)]
 pub(crate) struct Indexer<'item> {
     indexes: Indexes<'item>,
+    is_indexation_source_only: bool,
 }
 
 impl<'item> Indexer<'item> {
     pub(crate) fn run(modules: &'item [Module]) -> Indexes<'item> {
         let mut indexer = Self {
             indexes: Indexes::new(modules.len()),
+            is_indexation_source_only: false,
         };
         indexer.index_modules(modules);
         indexer.indexes
@@ -32,11 +35,9 @@ impl<'item> Indexer<'item> {
         for module in modules {
             self.index_module_items(module);
         }
+        self.index_consts_until_full_registration(modules);
         for module in modules {
-            self.index_module_signatures(module);
-        }
-        for module in modules {
-            self.index_module_refs(module);
+            self.index_not_consts(module);
         }
     }
 
@@ -77,38 +78,63 @@ impl<'item> Indexer<'item> {
         }
     }
 
-    fn index_module_signatures(&mut self, module: &'item Module) {
+    fn index_consts_until_full_registration(&mut self, modules: &'item [Module]) {
+        self.is_indexation_source_only = true;
+        // loop is used to support items referred in function signatures but defined later
+        loop {
+            let source_count = self.indexes.sources.len();
+            for module in modules {
+                self.index_consts(module);
+            }
+            if self.indexes.sources.len() == source_count {
+                break;
+            }
+        }
+        self.is_indexation_source_only = false;
+        for module in modules {
+            self.index_consts(module);
+        }
+    }
+
+    fn index_consts(&mut self, module: &'item Module) {
         for item in &module.items {
             match item {
-                Item::Fn(item) => {
-                    for param in &item.params.params {
-                        self.index_expr(&param.type_);
-                    }
-                    if let Some(return_type) = &item.return_type {
-                        self.index_expr(return_type);
-                    }
-                }
-                Item::Import(_)
-                | Item::Var(_)
-                | Item::Const(_)
-                | Item::Struct(_)
-                | Item::Repeat(_) => (),
+                Item::Const(item) => self.index_expr(&item.value),
+                Item::Fn(item) => self.index_fn_const_parts(item),
+                Item::Import(_) | Item::Var(_) | Item::Struct(_) | Item::Repeat(_) => (),
             }
         }
     }
 
-    fn index_module_refs(&mut self, module: &'item Module) {
+    fn index_not_consts(&mut self, module: &'item Module) {
         for item in &module.items {
             match item {
-                Item::Import(_) | Item::Struct(_) => (),
+                Item::Import(_) | Item::Struct(_) | Item::Const(_) => (),
                 Item::Var(item) => self.index_expr(&item.default_value),
-                Item::Const(item) => self.index_expr(&item.value),
-                Item::Fn(item) => {
-                    for statement in &item.statements {
-                        self.index_statement_refs(statement);
-                    }
-                }
+                Item::Fn(item) => self.index_fn_not_const_parts(item),
                 Item::Repeat(item) => self.index_call(&item.call),
+            }
+        }
+    }
+
+    fn index_fn_const_parts(&mut self, item: &'item FnDefinition) {
+        for param in &item.params.params {
+            self.index_expr(&param.type_);
+        }
+        if let Some(return_type) = &item.return_type {
+            self.index_expr(return_type);
+        }
+        if item.const_keyword_span.is_some() {
+            for statement in &item.statements {
+                self.index_statement_refs(statement);
+            }
+        }
+    }
+
+    fn index_fn_not_const_parts(&mut self, item: &'item FnDefinition) {
+        if item.const_keyword_span.is_none() {
+            for statement in &item.statements {
+                self.index_statement_refs(statement);
             }
         }
     }
@@ -135,6 +161,9 @@ impl<'item> Indexer<'item> {
     }
 
     fn index_call(&mut self, call: &'item Call) {
+        if self.is_indexation_source_only && self.indexes.sources.contains_key(&call.id) {
+            return;
+        }
         for arg in &call.args {
             self.index_expr(arg); // no-fn-check (recursivity)
         }
@@ -148,13 +177,16 @@ impl<'item> Indexer<'item> {
             },
         };
         if let Some(source) = self.search_accessible_call_source(call, search_params) {
-            self.index_source(&call, call.span, source);
+            self.index_accessible_source(&call, call.span, source);
         } else if let Some(source) = self.search_not_accessible_call_source(call, search_params) {
-            self.indexes.priv_sources.insert(call.id, source);
+            self.index_not_accessible_source(call.id, source);
         }
     }
 
     fn index_ident(&mut self, ident: &'item Ident) {
+        if self.is_indexation_source_only && self.indexes.sources.contains_key(&ident.id) {
+            return;
+        }
         let search_params = SearchParams {
             key: &ident.slice,
             location: ident,
@@ -166,9 +198,9 @@ impl<'item> Indexer<'item> {
         };
         let matching_value = self.search_accessible_ident_source(search_params);
         if let Some(source) = matching_value {
-            self.index_source(&ident, ident.span, source);
+            self.index_accessible_source(&ident, ident.span, source);
         } else if let Some(source) = self.search_not_accessible_ident_source(search_params) {
-            self.indexes.priv_sources.insert(ident.id, source);
+            self.index_not_accessible_source(ident.id, source);
         }
     }
 
@@ -217,8 +249,16 @@ impl<'item> Indexer<'item> {
             })
     }
 
-    fn index_source(&mut self, ref_: &impl NodeRef, ref_span: Span, source: ItemRef<'item>) {
+    fn index_accessible_source(
+        &mut self,
+        ref_: &impl NodeRef,
+        ref_span: Span,
+        source: ItemRef<'item>,
+    ) {
         self.indexes.sources.insert(ref_.id(), source);
+        if self.is_indexation_source_only {
+            return;
+        }
         self.indexes
             .imports
             .mark_as_used(ref_.file_index(), source.file_index());
@@ -226,5 +266,12 @@ impl<'item> Indexer<'item> {
             .item_first_refs
             .entry(source.id())
             .or_insert_with(|| ref_span);
+    }
+
+    fn index_not_accessible_source(&mut self, node_id: u64, source: ItemRef<'item>) {
+        if self.is_indexation_source_only {
+            return;
+        }
+        self.indexes.priv_sources.insert(node_id, source);
     }
 }
