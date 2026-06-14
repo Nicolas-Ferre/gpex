2d4644b91c6d5aa3b784714ea77f1b21559135d4
Nicolas Ferré <nicolas.ferre.public@gmail.com>
2026-06-14 17:50:08 +0200

    Refactor

--- src/compiler/transpilation/compilerimpl.rs
+++ src/compiler/transpilation/compilerimpl.rs
@@ -5,14 +5,16 @@ use crate::compiler::transpilation::Transpiler;
 impl Transpiler<'_, '_> {
     pub(super) fn transpile_compilerimpl_fn_call(&mut self, node: &Call, source: &FnDefinition) {
         match source.compilerimpl() {
-            Some(CompilerImpl::Add) => {
-                self.transpile_expr(&node.args[0]);
-                self.shader += " + ";
-                self.transpile_expr(&node.args[1]);
-            }
+            Some(CompilerImpl::Add) => self.transpile_compilerimpl_fn_call_add(node),
             Some(CompilerImpl::Typeof | CompilerImpl::Sizeof) | None => {
                 unreachable!("not implemented `{}` GPU function", source.name)
             }
         }
+    }
+
+    fn transpile_compilerimpl_fn_call_add(&mut self, node: &Call) {
+        self.transpile_expr(&node.args[0]);
+        self.shader += " + ";
+        self.transpile_expr(&node.args[1]);
     }
 }
