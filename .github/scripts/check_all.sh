a30f509b957c90975220f1b01cedf4a40afe8226
Nicolas Ferré <nicolas.ferre.public@gmail.com>
2026-06-19 19:19:01 +0200

    Add script to run all lints locally

---
+++ .github/scripts/check_all.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -euo pipefail
+
+pids=()
+commands=()
+
+command_name() {
+    local s="$*"
+    s="$(printf '%s' "$s" | sed -E 's/[^A-Za-z0-9_-]+/_/g; s/^_+//; s/_+$//')"
+    printf '%.50s\n' "$s"
+}
+
+start() {
+    echo "→ $*"
+    (set -xeuo pipefail && "$@") >"target/lints/lint-$(command_name "$*").log" 2>&1 &
+    pids+=("$!")
+    commands+=("$*")
+}
+
+mkdir -p target/lints
+
+cargo fmt
+shfmt --indent 4 -w .github/scripts/*.sh
+
+start cargo test --no-fail-fast
+start git diff --check
+start shellcheck .github/scripts/*.sh --shell bash --severity style --external-sources
+start cargo clippy --all-targets --no-deps -- -D warnings
+start bash .github/scripts/check_line_endings.sh
+start bash .github/scripts/check_todos.sh
+start bash .github/scripts/check_file_paths.sh
+start bash .github/scripts/check_impl_order.sh
+start bash .github/scripts/check_function_body_empty_lines.sh
+start bash .github/scripts/check_mod_location.sh
+start bash .github/scripts/check_function_call_order.sh
+start bash .github/scripts/check_function_visibility_order.sh
+
+failed=0
+for i in "${!pids[@]}"; do
+    command="${commands[$i]}"
+    if wait "${pids[$i]}"; then
+        echo "✓ $command"
+    else
+        echo "✗ $command"
+        cat "target/lints/lint-$command.log"
+        failed=1
+    fi
+done
+exit "$failed"
