467f0abde73c4825e19c62b12f18712dc735ca55
Nicolas Ferré <nicolas.ferre.public@gmail.com>
2026-07-18 20:11:04 +0200

    Skip line from test coverage

--- src/compiler/validation/validators/expr.rs
+++ src/compiler/validation/validators/expr.rs
@@ -89,7 +89,7 @@ pub(crate) fn check_mul_add_candidate(
     indexes: &Indexes<'_>,
 ) {
     let ItemRef::Fn(source) = source else {
-        return;
+        unreachable!("calls can only be functions")
     };
     if source.compilerimpl() != Some(CompilerImplFn::Binary(BinaryCompilerImplFn::Add))
         || !are_all_args_f32
