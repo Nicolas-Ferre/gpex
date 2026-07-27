1db46148cbbdede41465f4110d05e19485f611da
Nicolas Ferré <nicolas.ferre.public@gmail.com>
2026-07-27 15:42:52 +0200

    Refactor

--- src/compiler/validation/naming.rs
+++ src/compiler/validation/naming.rs
@@ -8,14 +8,14 @@ use crate::compiler::types;
 use crate::compiler::types::Type;
 use crate::compiler::validation::{ValidateState, logs};
 use crate::utils::parsing::span::{Span, SpanProps};
-use convert_case::{Boundary, Case as ConvertCase, Converter};
+use convert_case::{Boundary, Case, Converter};
 
-pub(super) const IMPORT_ALLOWED_CASES: &[Case] = &[Case::Snake];
-pub(super) const VAR_ALLOWED_CASES: &[Case] = &[Case::Snake];
+pub(super) const IMPORT_ALLOWED_CASES: &[Case<'static>] = &[Case::Snake];
+pub(super) const VAR_ALLOWED_CASES: &[Case<'static>] = &[Case::Snake];
 
 pub(super) fn validate_name(
     span: Span,
-    expected_cases: &[Case],
+    expected_cases: &[Case<'_>],
     state: &mut ValidateState<'_, '_>,
 ) {
     validate_char_count(span, state);
@@ -31,13 +31,16 @@ pub(super) fn validate_char_count(span: Span, state: &mut ValidateState<'_, '_>)
 
 pub(super) fn validate_case(
     span: Span,
-    expected_cases: &[Case],
+    expected_cases: &[Case<'_>],
     state: &mut ValidateState<'_, '_>,
 ) {
     let slice = state.context.slice(span);
-    if !expected_cases.iter().any(|case| case.is_valid(slice)) {
-        let case_labels = expected_cases.iter().map(|case| case.labels());
-        let replacements = expected_cases.iter().map(|case| case.convert(slice));
+    if !expected_cases
+        .iter()
+        .any(|case| is_case_valid(*case, slice))
+    {
+        let case_labels = expected_cases.iter().map(|case| case_label(*case));
+        let replacements = expected_cases.iter().map(|case| convert_name(*case, slice));
         state.add_log(logs::idents::invalid_case(
             slice,
             span,
@@ -51,18 +54,21 @@ pub(super) fn validate_case(
 pub(super) fn const_cases(
     const_: &ConstDefinition,
     state: &ValidateState<'_, '_>,
-) -> &'static [Case] {
+) -> &'static [Case<'static>] {
     let type_ = types::expr_type(&const_.value, state.inner);
     let is_typeref = state.inner.is_intrinsic_type(type_, IntrinsicType::Typeref);
     let may_be_typeref = type_.struct_ref().is_none() || is_typeref;
     if may_be_typeref {
-        &[Case::ScreamingSnake, Case::Pascal]
+        &[Case::UpperSnake, Case::Pascal]
     } else {
-        &[Case::ScreamingSnake]
+        &[Case::UpperSnake]
     }
 }
 
-pub(super) fn fn_cases(fn_: &FnDefinition, state: &ValidateState<'_, '_>) -> &'static [Case] {
+pub(super) fn fn_cases(
+    fn_: &FnDefinition,
+    state: &ValidateState<'_, '_>,
+) -> &'static [Case<'static>] {
     let type_ = types::fn_type(fn_, state.inner);
     let is_typeref = match type_ {
         Type::Struct(_) => state.inner.is_intrinsic_type(type_, IntrinsicType::Typeref),
@@ -79,7 +85,7 @@ pub(super) fn fn_cases(fn_: &FnDefinition, state: &ValidateState<'_, '_>) -> &'s
 pub(super) fn param_cases<'item>(
     param: &'item Param,
     state: &ValidateState<'_, 'item>,
-) -> &'static [Case] {
+) -> &'static [Case<'static>] {
     let type_ = types::param_type(param, state.inner);
     let is_typeref = state.inner.is_intrinsic_type(type_, IntrinsicType::Typeref);
     if is_typeref && param.const_mark_span().is_some() {
@@ -89,38 +95,28 @@ pub(super) fn param_cases<'item>(
     }
 }
 
-#[derive(Debug, Clone, Copy)]
-pub(super) enum Case {
-    Snake,
-    ScreamingSnake,
-    Pascal,
+fn case_label(case: Case<'_>) -> &'static str {
+    match case {
+        Case::Snake => "snake_case",
+        Case::UpperSnake => "SCREAMING_SNAKE_CASE",
+        Case::Pascal => "PascalCase",
+        _ => unreachable!("unsupported case: {case:?}"),
+    }
 }
 
-impl Case {
-    fn labels(self) -> &'static str {
-        match self {
-            Self::Snake => "snake_case",
-            Self::ScreamingSnake => "SCREAMING_SNAKE_CASE",
-            Self::Pascal => "PascalCase",
-        }
-    }
+fn is_case_valid(case: Case<'_>, slice: &str) -> bool {
+    BINARY_FN_NAMES.contains(&slice)
+        || UNARY_FN_NAMES.contains(&slice)
+        || convert_name(case, slice) == slice
+}
 
-    fn is_valid(self, slice: &str) -> bool {
-        BINARY_FN_NAMES.contains(&slice)
-            || UNARY_FN_NAMES.contains(&slice)
-            || self.convert(slice) == slice
-    }
-
-    fn convert(self, slice: &str) -> String {
-        let name = slice.trim_start_matches('_');
-        let converter = Converter::new().remove_boundaries(&Boundary::digits());
-        let converted_name = match self {
-            Self::Snake => converter.to_case(ConvertCase::Snake).convert(name),
-            Self::ScreamingSnake => converter.to_case(ConvertCase::UpperSnake).convert(name),
-            Self::Pascal => converter.to_case(ConvertCase::Pascal).convert(name),
-        };
-        let has_leading_underscore = slice.len() - name.len() > 0;
-        let underscore_prefixes = "_".repeat(has_leading_underscore.into());
-        format!("{underscore_prefixes}{converted_name}")
-    }
+fn convert_name(case: Case<'_>, slice: &str) -> String {
+    let name = slice.trim_start_matches('_');
+    let converter = Converter::new()
+        .remove_boundaries(&Boundary::digits())
+        .to_case(case);
+    let converted_name = converter.convert(name);
+    let has_leading_underscore = slice.len() - name.len() > 0;
+    let underscore_prefixes = "_".repeat(has_leading_underscore.into());
+    format!("{underscore_prefixes}{converted_name}")
 }
