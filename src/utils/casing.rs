939eb04080742fc9a34d84deb2c0e9d613ed37dc
Nicolas Ferré <nicolas.ferre.public@gmail.com>
2026-09-13 18:08:53 +0200

    Test script

--- src/utils/casing.rs
+++ src/utils/casing.rs
@@ -1,4 +1,7 @@
+use crate::Runner;
 use convert_case::{Boundary, Case, Converter};
+
+type _A = Runner;
 
 pub(crate) fn convert(name: &str, case: Case<'_>) -> String {
     let trimmed_name = name.trim_start_matches('_');
